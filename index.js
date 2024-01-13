ce737d137cf7a14c71d0eeec23e6e73e59bbb030
SFrasia <108404961+SFrasia@users.noreply.github.com>
2024-01-13 10:47:18 +0300

    Changes

---
+++ index.js
@@ -0,0 +1,15 @@
+function divide() {
+  2000 / 100; // => equal to 20
+}
+
+function square() {
+  let x = 2;
+  console.log(x * 2); // => equal to 4
+}
+
+function add(x, y) {
+  return x + y;
+}
+
+let result = add(3, 4);
+let result2 = add(result, 0);
